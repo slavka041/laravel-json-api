65104cb89491eb91a1849e1b329ee857a3484042
Vyacheslav Sinyakov <vyacheslav.s@cronix.ms>
2018-06-02 19:03:33 +0300

    code reformat

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,20 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication, RefreshDatabase;
+
+    public function signIn($user = null)
+    {
+        $user = $user ?? factory(User::class)->create();
+
+        $this->be($user, 'api');
+
+        return $this;
+    }
 }
