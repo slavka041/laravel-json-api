22d01d190940221726fa7be2069c2c2636f55b25
Vyacheslav Sinyakov <vyacheslav.s@cronix.ms>
2018-06-02 19:20:19 +0300

    add swagger documentation

---
+++ documentations/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @SWG\Swagger(
+ *     basePath="",
+ *     schemes={"http"},
+ *     @SWG\Info(
+ *         version="1.0",
+ *         title="API",
+ *     ),
+ *     @SWG\SecurityScheme(
+ *         type="apiKey",
+ *         securityDefinition="Bearer",
+ *         name="Authorization",
+ *         in="header"
+ *     )
+ * )
+ *
+ */
